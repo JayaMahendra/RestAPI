878b57f932ae677981bda8d41fbcbd072561399d
= <=>
2021-04-09 12:56:40 +0700

    bingung isi api key

---
+++ lib/service/http_service.dart
@@ -0,0 +1,7 @@
+import 'package:http/http.dart' as http;
+
+class HttpService {
+  final String apiKey = 'isi dengan api key anda';
+  final String baseUrl =
+      'https://developers.themoviedb.org/3/movies/get-popular-movies';
+}
